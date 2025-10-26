3d47ec9dd6efc9233867b7377ecc68fba8c1d105
hsdesarrollo <hsdesarrollo2025@gmail.com>
2025-10-26 20:48:35 -0300

    feat(content-types): add Ingreso and Egreso models with routes, controllers and services

---
+++ src/api/ingreso/routes/ingreso.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/ingresos',
+      handler: 'ingreso.find',
+      config: { auth: false },
+    },
+    {
+      method: 'GET',
+      path: '/ingresos/:id',
+      handler: 'ingreso.findOne',
+      config: { auth: false },
+    },
+    {
+      method: 'POST',
+      path: '/ingresos',
+      handler: 'ingreso.create',
+      config: { auth: false },
+    },
+    {
+      method: 'PUT',
+      path: '/ingresos/:id',
+      handler: 'ingreso.update',
+      config: { auth: false },
+    },
+    {
+      method: 'DELETE',
+      path: '/ingresos/:id',
+      handler: 'ingreso.delete',
+      config: { auth: false },
+    },
+  ],
+};
+
