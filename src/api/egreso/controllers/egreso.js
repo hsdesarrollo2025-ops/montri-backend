e7c21a4ca2f6826396dae231725f8d02770d3f93
hsdesarrollo <hsdesarrollo2025@gmail.com>
2025-10-28 21:46:32 -0300

    feat(Modificar los controladores de ingresos y egresos para que el campo usuario se asigne automáticamente con el usuario autenticado)

--- src/api/egreso/controllers/egreso.js
+++ src/api/egreso/controllers/egreso.js
@@ -2,5 +2,25 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::egreso.egreso');
+module.exports = createCoreController('api::egreso.egreso', ({ strapi }) => ({
+  async create(ctx) {
+    try {
+      const user = ctx.state.user;
+      if (!user) {
+        return ctx.unauthorized('Usuario no autenticado');
+      }
+
+      const { data } = ctx.request.body;
+      const newData = {
+        ...data,
+        usuario: user.id,
+      };
+
+      const response = await strapi.service('api::egreso.egreso').create({ data: newData });
+      return response;
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+}));
 
--- src/api/ingreso/controllers/ingreso.js
+++ src/api/ingreso/controllers/ingreso.js
@@ -2,5 +2,25 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::ingreso.ingreso');
+module.exports = createCoreController('api::ingreso.ingreso', ({ strapi }) => ({
+  async create(ctx) {
+    try {
+      const user = ctx.state.user;
+      if (!user) {
+        return ctx.unauthorized('Usuario no autenticado');
+      }
+
+      const { data } = ctx.request.body;
+      const newData = {
+        ...data,
+        usuario: user.id,
+      };
+
+      const response = await strapi.service('api::ingreso.ingreso').create({ data: newData });
+      return response;
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+}));
 
